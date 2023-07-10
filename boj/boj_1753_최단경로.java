602f4d826783705e4db52efa365808f4bfc72fb7
hyunw9 <rkdgusdnr32@naver.com>
2023-07-11 00:00:14 +0900

    added

---
+++ boj/boj_1753_최단경로.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1753_최단경로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+
+
+    }
+    
+}
