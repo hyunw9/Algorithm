07bae7763cff6ac645325d9a691fea91bdc71d29
hyunw9 <rkdgusdnr32@naver.com>
2023-07-31 23:53:01 +0900

    backtracking: nAndM9

---
+++ PS/Backtracking/boj_15663_N과M9.java
@@ -0,0 +1,68 @@
+package PS.Backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_15663_N과M9 {
+    static int[] arr;
+    static int N;
+    static int M;
+    static boolean[] used ;
+    static ArrayList<String> store ;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        used = new boolean[N];
+        store = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        bt("", 0);
+        System.out.println(sb);
+    }
+
+    public static void bt(String s, int cnt) {
+        //이전 코드, trim을 매 분기마다 시행하는 덕분에 시간 초과가 납니다.
+/*        if (cnt == M){
+            if(store.contains(s.trim())){
+                return;
+            }
+            else{
+                store.add(s.trim());
+                sb.append(s.trim()+"\n");
+                return;
+            }
+        }*/
+        if (cnt == M){
+            s=s.trim();
+            if(store.contains(s)){
+                return;
+            }
+            else{
+                store.add(s);
+                sb.append(s+"\n");
+                return;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(!used[i]){
+                used[i] = true;
+                bt(s+arr[i]+ " " , cnt+1);
+                used[i] = false;
+            }
+        }
+    }
+}
