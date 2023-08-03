272babf5914f23a957e31ed2ae8fa7dbcf81d7e8
hyunw9 <rkdgusdnr32@naver.com>
2023-08-03 13:36:36 +0900

    added

--- PS/DP/boj_2579_stairClimbing.java
+++ PS/DP/boj_2579_stairClimbing.java
@@ -27,7 +27,7 @@ public class boj_2579_stairClimbing {
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Arrays;    ㄴ
 import java.util.StringTokenizer;
 
 public class boj_2579_stairClimbing {
--- PS/Stack/boj_1946_신입사원.java
+++ PS/Stack/boj_1946_신입사원.java
@@ -18,11 +18,13 @@ public class boj_1946_신입사원 {
             HashMap<Integer,Integer> map = new HashMap<>();
             int scr = N;
 
+
             //해시맵에 key는 서류 심사 등수, value 는 면접 점수 등수로 추가
             for (int j = 0; j < N; j++) {
                 st= new StringTokenizer(br.readLine());
                 map.put(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
             }
+
             //hashmap은 오름차순으로 정렬되므로, i가 1이 아니라면 현재 인덱스의 값은 i-1 인덱스의 값이 존재하므로,
             //이미 서류 부분에서 한번 진 것이 됩니다. -> key 값을 비교해서, 이전 인덱스의 키 값보다 등수가 낮다면, 탈락입니다.
             for (int j = 1; j <= map.size(); j++) {
@@ -31,6 +33,7 @@ public class boj_1946_신입사원 {
                     //하지만 1 1 같은 반례가 생길 수 있으므로, 탈락한 사람의 인덱스의 값을 이전 인덱스의 키값으로 변경해줍니다.
                     map.put(j,map.get(j-1));
                     scr--;
+
                 }
             }
             sb.append(scr+"\n");
