afb72ca9fa1d092c6c9d4e7b1c3887813dbdb19a
hyunw9 <rkdgusdnr32@naver.com>
2023-08-08 23:41:43 +0900

    add 달리기 경주

---
+++ prgmrs/달리기경주.java
@@ -0,0 +1,17 @@
+package prgmrs;
+
+public class 달리기경주{
+    public static void main(String[] args) {
+        String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+        String[] callings = {"kai", "kai", "mine", "mine"};
+        solution(players,callings);
+
+    }
+        public static String[] solution(String[] players, String[] callings) {
+            String[] answer = {};
+            for (int i = 0; i < callings.length ; i++) {
+
+            }
+            return answer;
+        }
+}
