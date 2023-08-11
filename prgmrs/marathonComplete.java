308a93d203011a7bd9635f31a826b2431687a41c
hyunw9 <rkdgusdnr32@naver.com>
2023-08-12 08:32:39 +0900

    마라톤 완주

--- prgmrs/marathonComplete.java
+++ prgmrs/marathonComplete.java
@@ -6,25 +6,27 @@ public class marathonComplete {
     public static void main(String[] args) {
         String []participant = {"leo", "kiki", "eden"};
         String []completion = {"eden", "kiki"};
-        HashMap<String,Integer> status = new HashMap<>();
-        String answer = "";
-
-        for (String p : participant) {
-            status.put(p, status.getOrDefault(p, 0)+1);
-        }
-
-        for (String c : completion) {
-            status.put(c, status.get(c) - 1);
-        }
-
-        for (String key : status.keySet()) {
-            if (status.get(key) != 0) {
-                answer = key;
-                break;
-            }
-        }
-
-        System.out.println("nswer = " + answer);
+        solution(participant,completion);
     }
+        public static String solution(String[] participant, String[] completion) {
+            HashMap<String,Integer> status = new HashMap<>();
+            String answer = "";
+
+            for (String p : participant) {
+                status.put(p, status.getOrDefault(p, 0)+1);
+            }
+
+            for (String c : completion) {
+                status.put(c, status.get(c) - 1);
+            }
+
+            for (String key : status.keySet()) {
+                if (status.get(key) != 0) {
+                    answer = key;
+                    break;
+                }
+            }
+            return answer;
+        }
 
 }
