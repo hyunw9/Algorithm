cc92b3ae59f6002658707cc585f9976061ac86b7
hyunw9 <rkdgusdnr32@naver.com>
2023-09-15 00:02:42 +0900

    Create classAlgorithm.java

---
+++ PS/binarySearch/classAlgorithm.java
@@ -0,0 +1,35 @@
+package PS.binarySearch;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class classAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 25, 35, 45, 50, 55, 60, 75, 80, 90, 95};
+        int key = 50;
+
+        System.out.println(binarySearch(arr, key));
+
+    }
+
+    private static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] > key) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+        return -1;
+    }
+}
+
