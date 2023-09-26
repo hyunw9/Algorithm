950f0d051edde8c926cf0d1402873759eb08863d
hyunw9 <rkdgusdnr32@naver.com>
2023-09-27 00:18:43 +0900

    Update 특정숫자의빈도계산.java

--- PS/AlgorithmClass/특정숫자의빈도계산.java
+++ PS/AlgorithmClass/특정숫자의빈도계산.java
@@ -2,16 +2,16 @@ package PS.AlgorithmClass;
 
 public class 특정숫자의빈도계산 {
     public static void main(String[] args) {
-        findNum(4,12333,0);
+        System.out.println(findNum(3,12333,0));
     }
-    public static void findNum(int n,int num,int count){
+    public static int findNum(int n,int num,int count){
         if( num == 0 ) {
-            System.out.println(count);
-            return;
+            return count;
         }
         if( num % 10 == n) {
             count++;
         }
-        findNum(n,num/10,count);
+
+        return  findNum(n,num/10,count);
     }
 }
