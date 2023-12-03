603e8d051934e593a486baf472dd826ec5c9be8a
hyunw9 <rkdgusdnr32@naver.com>
2023-12-03 18:07:55 +0900

    Create boj_16953_AtoB.java

---
+++ PS/greedy/boj_16953_AtoB.java
@@ -0,0 +1,53 @@
+package PS.greedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj_16953_AtoB {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    init();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    int cnt = 0;
+    while(b!=a){
+      if(b<a){
+        cnt = -2;
+        break;
+      }
+
+      if(b % 2 == 0){
+        b = b/2;
+        cnt++;
+      }else if(b % 10 == 1){
+        b = b/10;
+        cnt++;
+      }
+      else{
+        cnt = -2;
+        break;
+      }
+    }
+    bw.write(cnt+1+"");
+    close();
+  }
+
+  public static void init(){
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  }
+
+  public static void close() throws IOException {
+    br.close();
+    bw.close();
+  }
+
+}
