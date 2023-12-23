1b6c22507ddd4e9db0ac1063e4c46a5bf8f5aa22
hyunw9 <rkdgusdnr32@naver.com>
2023-12-23 18:38:56 +0900

    Create Product_of_array_except_self.java

---
+++ PS/leetCode/ArraysHashing/Product_of_array_except_self.java
@@ -0,0 +1,25 @@
+package PS.leetCode.ArraysHashing;
+
+public class Product_of_array_except_self {
+  class Solution {
+    public int[] productExceptSelf(int[] nums) {
+      int [] answer = new int[nums.length];
+
+      int[] a = new int[nums.length];
+      int[] b = new int[nums.length];
+      a[0] = 1;
+      b[nums.length-1]= 1;
+      for(int i = 1; i < nums.length; i++){
+        a[i] += a[i-1] * nums[i-1];
+        b[nums.length-1-i] += b[nums.length-i] * nums[nums.length-i];
+      }
+      for(int i = 0 ; i < nums.length; i++){
+        answer[i] = a[i] * b[i];
+      }
+
+      return answer;
+    }
+  }
+
+
+}
