27cf6fba0912db6b7e847fa3a343a172d91eec03
hyunw9 <rkdgusdnr32@naver.com>
2023-12-27 22:12:58 +0900

    Create Container_with_most_water.java

---
+++ PS/leetCode/TwoPointers/Container_with_most_water.java
@@ -0,0 +1,24 @@
+package PS.leetCode.TwoPointers;
+
+public class Container_with_most_water {
+  class Solution {
+    public int maxArea(int[] height) {
+      int max = 0;
+      int l = 0;
+      int r = height.length-1;
+      int val= 0;
+      while(l<=r){
+        int d = r-l;
+        val = Math.min(height[r],height[l]);
+        max = Math.max(max,d * val);
+        if(height[l]<height[r]){
+          l++;
+        }else{
+          r--;
+        }
+      }
+      return max;
+    }
+  }
+
+}
