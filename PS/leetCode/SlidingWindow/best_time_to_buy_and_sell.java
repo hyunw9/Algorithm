69c2ed54fd767c31cac64cf86c9feab394cc065b
hyunw9 <rkdgusdnr32@naver.com>
2023-12-28 23:21:27 +0900

    Create best_time_to_buy_and_sell.java

---
+++ PS/leetCode/SlidingWindow/best_time_to_buy_and_sell.java
@@ -0,0 +1,15 @@
+package PS.leetCode.SlidingWindow;
+
+public class best_time_to_buy_and_sell {
+
+  public int maxProfit(int[] prices) {
+    int minprice =Integer.MAX_VALUE;
+    int maxProfit = 0;
+    for(int price : prices){
+      minprice = Math.min(minprice,price);
+      maxProfit = Math.max(maxProfit,price-minprice);
+    }
+    return maxProfit;
+  }
+
+}
