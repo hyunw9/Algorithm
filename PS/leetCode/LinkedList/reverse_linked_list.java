9f985e8193d6fd3d1e1e137d1d74d4d7b75f8148
hyunw9 <rkdgusdnr32@naver.com>
2024-01-04 00:19:25 +0900

    Create reverse_linked_list.java

---
+++ PS/leetCode/LinkedList/reverse_linked_list.java
@@ -0,0 +1,41 @@
+package PS.leetCode.LinkedList;
+
+public class reverse_linked_list {
+
+  public ListNode reverseList(ListNode head) {
+    if (head == null) {
+      return head;
+    }
+    ListNode cur = head;
+    ListNode prev = null;
+    ListNode tmp = null;
+    while (cur != null) {
+
+      tmp = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = tmp;
+
+    }
+    return prev;
+  }
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+}
