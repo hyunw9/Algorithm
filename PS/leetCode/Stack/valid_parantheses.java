f007733499410989a336a16756ee62ceb4b6f01b
hyunw9 <rkdgusdnr32@naver.com>
2024-01-03 16:01:07 +0900

    Create valid_parantheses.java

---
+++ PS/leetCode/Stack/valid_parantheses.java
@@ -0,0 +1,40 @@
+package PS.leetCode.Stack;
+
+import java.util.Stack;
+
+public class valid_parantheses {
+
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for(int i = 0;i<s.length(); i++){
+      char now = s.charAt(i);
+      if(now == '(' || now =='{' || now == '['){
+        stack.push(now);
+      }else if(now == ')'){
+        if(!stack.isEmpty() && stack.peek()=='('){
+          stack.pop();
+        }else{
+          return false;
+        }
+      }else if(now == '}'){
+        if(!stack.isEmpty() && stack.peek()=='{'){
+          stack.pop();
+        }else{
+          return false;
+        }
+      }else if(now == ']'){
+        if(!stack.isEmpty() && stack.peek() == '['){
+          stack.pop();
+        }else{
+          return false;
+        }
+      }
+    }
+    if(stack.isEmpty()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+}
