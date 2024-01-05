714cc60cfda976c832a9a20a840e569da87237fd
hyunw9 <rkdgusdnr32@naver.com>
2024-01-05 10:32:34 +0900

    Create boj_11403_경로찾기.java

---
+++ PS/graph/boj_11403_경로찾기.java
@@ -0,0 +1,61 @@
+package PS.graph;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_11403_경로찾기 {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    init();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int[][] map = new int[n + 1][n + 1];
+    for (int i = 1; i <= n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 1; j <= n; j++) {
+        int num = Integer.parseInt(st.nextToken());
+        if (num == 1) {
+          map[i][j] = 1;
+        }
+      }
+    }
+    for (int k = 1; k <= n; k++) {
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+          if (map[i][j] == 0) {
+            if (map[i][k] + map[k][j] == 2) {
+              map[i][j] = 1;
+            }
+          }
+        }
+
+      }
+    }
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        bw.write(map[i][j] + " ");
+      }
+      bw.write("\n");
+    }
+    close();
+  }
+
+  public static void init() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  }
+
+  public static void close() throws IOException {
+    br.close();
+    bw.close();
+  }
+
+}
