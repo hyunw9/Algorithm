72435a46994ea4a7b1bb8bf74dcda2efb2729e42
hyunw9 <rkdgusdnr32@naver.com>
2024-01-05 21:11:43 +0900

    Create remove_nth_node_from_end_of_list.java

---
+++ PS/leetCode/LinkedList/remove_nth_node_from_end_of_list.java
@@ -0,0 +1,42 @@
+package PS.leetCode.LinkedList;
+
+public class remove_nth_node_from_end_of_list {
+
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode first = head;
+    ListNode last = head;
+    for (int i = 0; i < n; i++) {
+      first = first.next;
+    }
+    if (first == null) {
+      return head.next;
+    }
+    while (first.next != null) {
+      first = first.next;
+      last = last.next;
+    }
+    last.next = last.next.next;
+    return head;
+  }
+
+  public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+}
+
+
