73e31b91589629729641f3fe68d11fea51759e7a
hyunw9 <rkdgusdnr32@naver.com>
2024-01-08 15:08:28 +0900

    Create Merged_k_sorted_lists.java

---
+++ PS/leetCode/LinkedList/Merged_k_sorted_lists.java
@@ -0,0 +1,29 @@
+package PS.leetCode.LinkedList;
+
+import PS.leetCode.LinkedList.linked_list_cycle.ListNode;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Merged_k_sorted_lists {
+
+  public ListNode mergeKLists(ListNode[] lists) {
+    if(lists.length == 0) return null;
+
+    PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(x-> x.val));
+    for(ListNode node: lists){
+
+      if(node != null) pq.add(node);
+    }
+    ListNode curr = new ListNode(-1);
+    ListNode head = curr;
+    while(!pq.isEmpty()){
+      ListNode node = pq.poll();
+      curr.next = node;
+      curr = curr.next;
+      if(node.next != null) pq.add(node.next);
+    }
+
+    return head.next;
+  }
+
+}
