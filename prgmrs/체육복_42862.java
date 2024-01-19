926d005655e5365df8afc0fb2a00486622e2ff45
hyunw9 <rkdgusdnr32@naver.com>
2024-01-19 11:15:05 +0900

    Create 체육복_42862.java

---
+++ prgmrs/체육복_42862.java
@@ -0,0 +1,37 @@
+package prgmrs;
+
+import java.util.Arrays;
+
+public class 체육복_42862 {
+
+  public int solution(int n, int[] lost, int[] reserve) {
+    int answer = n-lost.length;
+    Arrays.sort(lost);
+    Arrays.sort(reserve);
+
+    for(int i = 0 ; i < lost.length ; i++){
+
+      for(int j = 0 ; j < reserve.length ;j++){
+
+        if(reserve[j] == lost[i]){
+          answer++;
+          reserve[j] = -1;
+          lost[i]= -1;
+          break;
+        }
+      }
+    }
+    for(int i = 0 ; i < lost.length ; i++){
+      for(int j = 0 ; j < reserve.length ;j++){
+        if(lost[i]+1 == reserve[j] || lost[i]-1 ==reserve[j]){
+          //lost[i] = -1;
+          reserve[j]=-1;
+          answer++;
+          break;
+        }
+      }
+    }
+    return answer;
+  }
+
+}
