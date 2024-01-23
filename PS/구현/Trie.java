92a5f5d3a22ca509ffbbc0cc8a3696fa7251a827
hyunw9 <rkdgusdnr32@naver.com>
2024-01-24 00:47:33 +0900

    Create Trie.java

---
+++ PS/구현/Trie.java
@@ -0,0 +1,50 @@
+package PS.구현;
+
+import java.util.HashMap;
+
+public class asd {
+
+  private static int answer ;
+
+  static class Node{
+
+    private  HashMap<Character, Node> child;
+    private  int cnt ;
+
+    public Node(){
+      child = new HashMap<>();
+    }
+  }
+
+  public static int solution(String[] words) {
+    Node root = new Node();
+    for(int i = 0; i<words.length; i++){
+      insert(root,words[i]);
+    }
+    for(int i = 0 ; i < words.length; i++){
+      check(root,words[i]);
+    }
+    return answer;
+  }
+
+  public static void insert(Node curr, String str){
+    for(char a : str.toCharArray()){
+      curr = curr.child.computeIfAbsent(a,l -> new Node());
+      curr.cnt++;
+    }
+  }
+
+  public static void check(Node curr, String str){
+    for( char a : str.toCharArray()){
+      curr = curr.child.get(a);
+      answer++;
+      if(curr.cnt == 1) return;
+    }
+  }
+
+  public static void main(String[] args){
+    String[] a = {"123","1234","12345"};
+    System.out.println(solution(a));
+  }
+
+}
