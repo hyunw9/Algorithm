94f2419e99b477cefc9cd1ba8d47724cac627027
hyunw9 <rkdgusdnr32@naver.com>
2024-01-25 15:55:39 +0900

    Create boj_11727_2n타일링2.java

---
+++ PS/DP/boj_11727_2n타일링2.java
@@ -0,0 +1,38 @@
+package PS.DP;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class boj_11727_2n타일링2 {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    init();
+    int n = Integer.parseInt(br.readLine());
+    int[] dp= new int[1001];
+    dp[1]=1;
+    dp[2] = 3;
+    for(int i = 3; i<=1000;i++){
+      dp[i] = (dp[i-1] +2*dp[i-2])%10007;
+    }
+    bw.write(dp[n]+"");
+    close();
+  }
+
+  public static void init() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  }
+
+  public static void close() throws IOException {
+    br.close();
+    bw.close();
+  }
+
+}
