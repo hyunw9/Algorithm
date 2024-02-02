8f4cc52a48da5badbc46b94841739f475f739eda
hyunw9 <rkdgusdnr32@naver.com>
2024-02-02 10:53:41 +0900

    Create K번째수_42748.java

---
+++ prgmrs/Sort/K번째수_42748.java
@@ -0,0 +1,28 @@
+package prgmrs.Sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class K번째수_42748 {
+
+  public int[] solution(int[] array, int[][] commands) {
+
+    int []answer= new int[commands.length];
+    for (int i = 0; i <commands.length; i++) {
+      ArrayList<Integer> arr = new ArrayList<>();
+      int to = commands[i][0];
+      int end = commands[i][1];
+      int target = commands[i][2];
+      for(int j = to-1; j<=end-1; j++){
+        arr.add(array[j]);
+      }
+      Collections.sort(arr);
+      //System.out.println(arr);
+
+      answer[i]=arr.get(target-1);
+
+    }
+    return answer;
+  }
+
+}
