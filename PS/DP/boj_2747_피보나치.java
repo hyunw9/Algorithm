0db7f914f3148bbf61e35aadb8c25fbd7108970f
hyunw9 <rkdgusdnr32@naver.com>
2024-02-10 00:01:51 +0900

    Create boj_2747_피보나치.java

---
+++ PS/DP/boj_2747_피보나치.java
@@ -0,0 +1,39 @@
+package PS.DP;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj_2747_피보나치 {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    init();
+    int n = Integer.parseInt(br.readLine());
+
+    int[] dp = new int[46];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i = 2 ; i < dp.length;i++){
+      dp[i] = dp[i-1]+dp[i-2];
+    }
+    bw.write(dp[n-1]+"");
+    close();
+  }
+
+  public static void init() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  }
+
+  public static void close() throws IOException {
+    br.close();
+    bw.close();
+  }
+
+}
