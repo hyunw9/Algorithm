2d85d2248921e773b8cfda6bfb086783e7a35a40
hyunw9 <rkdgusdnr32@naver.com>
2024-02-27 01:14:27 +0900

    Create boj_2467_용액.java

---
+++ PS/binarySearch/boj_2467_용액.java
@@ -0,0 +1,68 @@
+package PS.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj_2467_용액 {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    init();
+    //0에 가까운걸 출력해야함,
+    //어떻게 0에 가까운걸 판단하지 ?
+
+    //더해서 , 절댓값이 작은 것 순으로 정렬해놓으면 됮 ㅣ않나?
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+
+    int [] arr = new int[n];
+
+    st= new StringTokenizer(br.readLine());
+    for(int i = 0; i<n; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    int l = 0;
+    int r =  n-1;
+    int answer= Integer.MAX_VALUE;
+    int ansl=0;
+    int ansr=0;
+    while(l<r){
+      int mid = (l+r)/2;
+
+      int sum = arr[l]+arr[r];
+
+      if(Math.abs(sum) <= answer){
+        answer = Math.abs(sum);
+
+        ansl = l;
+        ansr = r;
+      }
+
+      if(sum>=0){//0보다 크다면
+        r=r-1;
+
+      }else{ //0보다 작다면
+        l=l+1;
+      }
+    }
+    bw.write(arr[ansl] +" "+ arr[ansr]);
+    close();
+  }
+
+  public static void init() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  }
+
+  public static void close() throws IOException {
+    br.close();
+    bw.close();
+  }
+
+}
