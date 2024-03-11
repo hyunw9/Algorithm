1eb8a90a0bb7ed113a97b16fab0d9ffc1f646dc6
hyunw9 <rkdgusdnr32@naver.com>
2024-03-11 18:24:22 +0900

    Create boj_13909_창문닫기.java

---
+++ PS/Math/boj_13909_창문닫기.java
@@ -0,0 +1,31 @@
+package PS.Math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj_13909_창문닫기 {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    init();
+    int n = Integer.parseInt(br.readLine());
+    System.out.println((int)Math.sqrt(n));
+    close();
+  }
+
+  public static void init() {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  }
+
+  public static void close() throws IOException {
+    br.close();
+    bw.close();
+  }
+
+}
