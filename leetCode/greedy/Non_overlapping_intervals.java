2edf5af0b856e5bdf4a8f7b242de1c1ebc22655e
hyunw9 <rkdgusdnr32@naver.com>
2024-04-17 15:52:57 +0900

    Update Non_overlapping_intervals.java

--- leetCode/greedy/Non_overlapping_intervals.java
+++ leetCode/greedy/Non_overlapping_intervals.java
@@ -3,19 +3,19 @@ package leetCode.greedy;
 import java.util.Arrays;
 
 public class Non_overlapping_intervals {
- public int eraseOverlapIntervals(int[][] intervals) {
-        int n = intervals.length;
-        Arrays.sort(intervals,(o1,o2)->Integer.compare(o1[1],o2[1]));
+  public int eraseOverlapIntervals(int[][] intervals) {
+    int n = intervals.length;
+    Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[1], o2[1]));
 
-        int count = 1;
-        int prev = 0;
+    int count = 1;
+    int prev = 0;
 
-        for(int i = 1; i <n ; i ++){
-            if(intervals[i][0]>=intervals[prev][1]){
-                prev = i;
-                count ++;
-            }
-        }
-        return n - count;
-    }   
+    for (int i = 1; i < n; i++) {
+      if (intervals[i][0] >= intervals[prev][1]) {
+        prev = i;
+        count++;
+      }
+    }
+    return n - count;
+  }
 }
