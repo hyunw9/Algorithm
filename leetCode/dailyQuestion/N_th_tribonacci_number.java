0ef9c29e90b320a8d32abae4af9aa52035f44de4
hyunw9 <rkdgusdnr32@naver.com>
2024-04-24 19:28:59 +0900

    Create N_th_tribonacci_number.java

---
+++ leetCode/dailyQuestion/N_th_tribonacci_number.java
@@ -0,0 +1,26 @@
+package leetCode.dailyQuestion;
+
+public class N_th_tribonacci_number {
+
+  public int tribonacci(int n) {
+    if (n < 3) {
+      if (n == 1) {
+        return 1;
+      } else if (n == 2) {
+        return 1;
+      } else if (n == 0) {
+        return 0;
+      }
+    }
+
+    int[] dp = new int[n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    return dp[n];
+  }
+
+}
