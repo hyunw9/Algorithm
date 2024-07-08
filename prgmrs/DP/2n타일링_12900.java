f8038864acafdb6b751fa1856643ff80c1bab0c7
hyunw9 <rkdgusdnr32@naver.com>
2024-07-08 13:23:56 +0900

    Create 2n타일링_12900.java

---
+++ prgmrs/DP/2n타일링_12900.java
@@ -0,0 +1,30 @@
+package prgmrs.DP;
+
+public class 2n타일링_12900 {
+    import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        //n == 1
+        //  1 
+        //  n==2 2
+        //  2 
+        //  n==3
+        //  l l l , --l, l-- 3
+        //  n == 4
+        //  llll, l--l, ll--, --ll, ---- 5
+        //  
+        int[] dp = new int[n+1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        int answer = 0;
+        for(int i = 2; i < dp.length ; i ++){
+            dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        }
+        // System.out.println(Arrays.toString(dp));
+        return dp[n];
+    }
+
+}
