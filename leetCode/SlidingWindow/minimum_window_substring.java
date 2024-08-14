531292691ec99926e1e239a9e6a77a65e1d12163
hyunw9 <rkdgusdnr32@naver.com>
2024-08-14 12:01:53 +0900

    Update minimum_window_substring.java

--- leetCode/SlidingWindow/minimum_window_substring.java
+++ leetCode/SlidingWindow/minimum_window_substring.java
@@ -1,41 +1,64 @@
 package leetCode.SlidingWindow;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class minimum_window_substring {
 
   class Solution {
-
     public String minWindow(String s, String t) {
-      String sub = s;
+      if (s.length() < t.length()) return "";
       HashMap<Character, Integer> map = new HashMap<>();
-      for (int i = 0; i < t.length(); i++) {
-        map.put(t.charAt(i), 1);
+
+      for (char b : t.toCharArray()) {
+
+        map.put(b, map.getOrDefault(b, 0) + 1);
       }
+
+      int matched = 0;
       int l = 0;
       int r = 0;
-      int window = 0;
-      int count = 0;
-      for (; r < s.length(); r++) {
-        char now = s.charAt(r);
-        window = r - l + 1;
-        if (map.get(now) != null && map.get(now) > 0) {
-          count += 1;
-          map.put(now, map.get(now) - 1);
-        }
-        if (count > t.length()) {
-          if (map.containsKey(s.charAt(l))) {
-            map.put(s.charAt(l), map.get(s.charAt(l)) + 1);
-            l++;
-            count--;
-          } else if (!map.containsKey(s.charAt(l))) {
-            l++;
+      List<int[]> arr = new ArrayList<>();
+      int minSize = 987654321;
+      while (l <= r && r < s.length()) {
+
+        char right = s.charAt(r);
+
+        if (map.containsKey(right)) {
+          map.put(right, map.get(right) - 1);
+          if (map.get(right) == 0) {
+            matched += 1;
           }
         }
 
+        while (matched == map.size()) {
 
+          int window = r - l + 1;
+          if (window < minSize) {
+            arr.add(new int[] {l, r});
+          }
+
+          char left = s.charAt(l);
+          if (map.containsKey(left)) {
+            map.put(left, map.get(left) + 1);
+            if (map.get(left) > 0) {
+              matched -= 1;
+            }
+          }
+          l++;
+        }
+        r++;
       }
-      return sub;
+      // System.out.println(map);
+      Collections.sort(arr, (o1, o2) -> (o1[1] - o1[0]) - (o2[1] - o2[0]));
+      if (arr.size() == 0) {
+        return "";
+      }
+
+      int[] result = arr.get(0);
+      return s.substring(result[0], result[1] + 1);
     }
   }
 }
