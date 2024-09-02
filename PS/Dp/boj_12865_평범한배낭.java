3490a26efd21e9b585ebe416d52e405eae5878a0
hyunw9 <rkdgusdnr32@naver.com>
2024-09-03 00:44:19 +0900

    Update boj_12865_평범한배낭.java

--- PS/Dp/boj_12865_평범한배낭.java
+++ PS/Dp/boj_12865_평범한배낭.java
@@ -1,75 +1,58 @@
 package PS.Dp;
 // package PS.DP;
 
-// import java.io.*;
-// import java.util.*;
+import java.io.*;
+import java.util.*;
 
-// public class boj_12865_평범한배낭 {
+public class boj_12865_평범한배낭 {
 
-//     private static BufferedReader br;
-//     private static BufferedWriter bw;
-//     private static int n ;
-//     private static int w ;
-//     private static int v ;
-//     private static int max;
-//     private static boolean[] used;
-//     private static Item[] list ;
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static int n;
+    private static int K;
+    private static int[]w;
+    private static int[]v;
+    private static int[][]dp;
 
-//     private static class Item{
-//         int weight;
-//         int val;
-//         public Item(int weight, int val){
-//             this.weight = weight;
-//             this.val = val;
-//         }
-//     }
 
-//     public static void main(String[] args) throws IOException {
-//         init();
-//         StringTokenizer st = new StringTokenizer(br.readLine());
-//          n = Integer.parseInt(st.nextToken());
-//          w = Integer.parseInt(st.nextToken());
-//          v = 0;
-//          used = new boolean[n];
-//         list  = new Item[n];
-//         for (int i = 0; i < n; i++) {
-//             st = new StringTokenizer(br.readLine());
-//             int wei = Integer.parseInt(st.nextToken());
-//             int val = Integer.parseInt(st.nextToken());
-//             Item now = new Item(wei,val);
-//             list[i] = now;
-//         }
-//         dfs(0,0,w);
-//         bw.write(max+"");
-//         close();    
-//     }
+    public static void main(String[] args) throws IOException {
+        init();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-//     private static void dfs(int depth, int currVal,int currWei){
-//         if(depth == n|| w<0 ){
-//             // System.out.println(Arrays.toString(used));
-//             max = Math.max(max,currVal);
-//             return;
-//         }
-//         for (int i = depth; i < used.length; i++) {
-//             if(!used[i]){
-//                 used[i] = true;
-//                 if(currWei - list[i].weight>=0){
-//                     dfs(i+1,currVal+ list[i].val,currWei-list[i].weight);
-//                 }else{
-//                     dfs(depth+1,currVal,currWei);
-//                 }
-//                 used[i]= false;
-//             }
-//         }
-//     }
+        w= new int[n+1];
+        v= new int[n+1];
+        dp = new int[n+1][K+1];
 
-//     private static void init() throws IOException {
-//         br = new BufferedReader(new InputStreamReader(System.in));
-//         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//     }
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(Arrays.toString(w));
+        for(int i = 1 ; i <= n; i++){
+            for(int k = 1; k<= K; k++ ){
+                if(w[i-1]<=k){
+                    dp[i][k] = Math.max(dp[i-1][k],dp[i-1][k-w[i-1]]+v[i-1]);
+                }else{
+                    dp[i][k] = dp[i-1][k];
+                }
+            }
+        }
+        System.out.println(dp[n][K]);
 
-//     private static void close() throws IOException {
-//         br.close();
-//         bw.close();
-//     }
-// }
+        
+        close();    
+    }
+
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    private static void close() throws IOException {
+        br.close();
+        bw.close();
+    }
+}
