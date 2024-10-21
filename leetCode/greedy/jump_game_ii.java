3ac310b872b9971895d60330a500cfbbbee3d947
hyunw9 <rkdgusdnr32@naver.com>
2024-10-21 14:38:35 +0900

    Update jump_game_ii.java

--- leetCode/greedy/jump_game_ii.java
+++ leetCode/greedy/jump_game_ii.java
@@ -30,5 +30,26 @@ public class jump_game_ii {
     }
   }
 
+  public int jump(int[] nums) {
+    int res  = 0 ;
+    int l = 0; 
+    int r = 0;
+
+    while( r < nums.length-1){
+
+        int max = 0;
+
+        for(int i  = l; i<=r; i++){
+            max= Math.max(max,i + nums[i]);
+        }
+
+        l = r+1;
+        r = max;
+        res++;
+    }
+
+    return res;
+}
+
   // 2 + 2 > 3 ;
 }
