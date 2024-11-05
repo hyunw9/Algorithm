9b68031d1d59a3773f75509bf5ca4c4813a10f68
hyunw9 <rkdgusdnr32@naver.com>
2024-11-05 09:41:15 +0900

    Update boj_5427_불.java

--- PS/BFS/boj_5427_불.java
+++ PS/BFS/boj_5427_불.java
@@ -1,129 +1,112 @@
 package PS.BFS;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import leetCode.dailyQuestion.open_the_lock.Pair;
+import java.io.*;
+import java.util.*;
 
 public class boj_5427_불 {
 
-  static char[][] map;
-  private static BufferedReader br;
-  private static BufferedWriter bw;
-  private static int[] dx = {0, 0, -1, 1};
-  private static int[] dy = {-1, 1, 0, 0};
-  private static int wall = -1;
-  private static int fire = 0;
-  private static int path = 1;
-  private static int sangeun = 2;
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static String[][] grid; 
+    private static int[] dx ={0,0,-1,1};
+    private static int[] dy = {-1,1,0,0};
+    private static int[][] cost;
+    private static Queue<int[]> fq;
+    private static Queue<int[]> sq;
+    private static int escape ;
 
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+      init();
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int t = Integer.parseInt(st.nextToken());
+      while(--t>=0){  
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        grid = new String[n][m];
+        cost = new int[n][m];
+        fq = new LinkedList<>();
+        sq = new LinkedList<>();
 
-    init();
-    int t = Integer.parseInt(br.readLine());
-    while (--t >= 0) {
-      String line = br.readLine();
-      int m = Integer.parseInt(line.split(" ")[0]);
-      int n = Integer.parseInt(line.split(" ")[1]);
-      int[][] map = new int[n][m];
-      for (int i = 0; i < n; i++) {
-        line = br.readLine();
-        for (int j = 0; j < m; j++) {
-          char now = line.charAt(j);
-          if (now == '#') {
-            map[i][j] = wall;
-          } else if (now == '*') {
-            map[i][j] = fire;
-          } else if (now == '.') {
-            map[i][j] = path;
-          } else {
-            map[i][j] = sangeun;
-          }
-        }
-      }
-      Queue<Node> q = new LinkedList<>();
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-          if (map[i][j] == fire) {
-            q.add(new Node(i, j, 0, fire));
-          }
-        }
-      }
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-          if (map[i][j] == sangeun) {
-            q.add(new Node(i, j, 0, sangeun));
-          }
-        }
-      }
-      boolean flag = false;
+        for(int i =0; i<n; i++){
+          String line = br.readLine();
+          for(int j = 0 ; j < m ; j++){
+            grid[i][j] = String.valueOf(line.charAt(j));
 
-      while (!q.isEmpty()) {
-        Node now = q.poll();
-        for (int i = 0; i < 4; i++) {
-
-          int ny = dy[i] + now.y;
-          int nx = dx[i] = now.x;
-
-          if (ny >= 0 && nx >= 0 && ny < n && nx < m) {
-            if (now.state == fire && map[ny][nx] != wall && map[ny][nx] != fire) {
-              q.add(new Node(ny, nx, 0, fire));
-              map[ny][nx] = fire;
-            } else if (now.state == sangeun && map[ny][nx] == path) {
-              q.add(new Node(ny, nx, now.value + 1, sangeun));
-              map[ny][nx] = sangeun;
-            }
-          } else {
-            if(now.state == sangeun){
-              bw.write((now.value+1)+"\n");
-              System.out.println(now.value+1);
-              flag = true;
-              break;
+            if(grid[i][j].equals("*")){
+              cost[i][j] = 1;
+              fq.offer(new int[]{i,j});
+              
+            }else if(grid[i][j].equals("@")){
+              sq.offer(new int[]{i,j,1});
             }
           }
         }
-        if (flag) {
-          //탈출 성공 !
-          break;
+
+        escape = bfs(n,m);
+        if(escape == -1){
+          bw.write("IMPOSSIBLE\n");
+        }else{
+          bw.write(escape+"\n");
         }
       }
-      if (!flag) {
-        bw.write("IMPOSSIBLE\n");
+        close();
+    }
+
+    private static int bfs(int n,int m){
+
+      while(!fq.isEmpty()){
+
+        int[] curr = fq.poll();
+
+        int y = curr[0];
+        int x = curr[1];
+
+        for(int i = 0; i < 4; i++){
+          int ny = dy[i] + y;
+          int nx = dx[i]+ x;
+
+          if(ny<0 || nx < 0 || ny>=n || nx>=m||cost[ny][nx]>0||grid[ny][nx].equals("#")) continue;
+          cost[ny][nx] = cost[y][x]+1;
+          fq.offer(new int[]{ny,nx});
+        }
       }
-      System.out.println(Arrays.deepToString(map));
+      System.out.println(Arrays.deepToString(cost));
+      while(!sq.isEmpty()){
+        int [] curr = sq.poll();
+        int y = curr[0];
+        int x = curr[1];
+        int c = curr[2];
+        // System.out.println("y : "+ y + " x : "+ x + " c : "+ c);
 
+        if(y<=0 ||x <=0 || y >= n-1 || x>=m-1){
+          return c;
+        }
+
+        for(int i = 0; i < 4; i++){
+          int ny = dy[i]+y;
+          int nx = dx[i]+x;
+
+          if(ny<0 || nx < 0 || ny>=n || nx>=m||(cost[ny][nx]>0 && cost[ny][nx]<=c+1)||grid[ny][nx].equals("#")) continue;
+          cost[ny][nx] = c+1;
+          sq.offer(new int[]{ny,nx,c+1});
+        }
+      }
+
+      return -1;
     }
-    close();
-  }
 
-  public static void init() {
-    br = new BufferedReader(new InputStreamReader(System.in));
-    bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  }
-
-  public static void close() throws IOException {
-    br.close();
-    bw.close();
-  }
-
-  static class Node {
-
-    int y;
-    int x;
-    int value;
-    int state;
-
-    public Node(int y, int x, int value, int state) {
-      this.y = y;
-      this.x = x;
-      this.value = value;
-      this.state = state;
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
     }
-  }
 
+    private static void close() throws IOException {
+        br.close();
+        bw.close();
+    }
 }
+
+/*
+** 간략한 풀이 :
+*/
