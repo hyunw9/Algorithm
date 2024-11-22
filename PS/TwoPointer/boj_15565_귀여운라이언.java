a2f971eb50007c8021419e02adfd960e4baff12b
hyunw9 <rkdgusdnr32@naver.com>
2024-11-22 12:12:49 +0900

    Update boj_15565_귀여운라이언.java

--- PS/TwoPointer/boj_15565_귀여운라이언.java
+++ PS/TwoPointer/boj_15565_귀여운라이언.java
@@ -13,7 +13,7 @@ public class boj_15565_귀여운라이언 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
@@ -24,21 +24,20 @@ public class boj_15565_귀여운라이언 {
         int r = 0;
         int cnt = 0;
         int min = Integer.MAX_VALUE;
-        while(l<=r){
-            System.out.println(l+ " " + r);
-            if(cnt<m){
-                if(arr[++r]==1){
-                    cnt+=1;
-                }
-            }else{
-                if(cnt==m){
-                    min = Math.min(min,r-l+1);
-                    if(arr[l++]==1){
-                        cnt-=1;
-                    }
-                }
+        while(r<n){
+            // System.out.println(l+ " " + r+ " "+ cnt);
+            
+            if(arr[r] == 1) cnt+=1;
+
+            while(cnt>=k){
+                min = Math.min(min,r-l+1);
+                if(arr[l]==1) cnt-=1;
+                l+=1;
             }
+            r+=1;
+            
         }
+        if(min == Integer.MAX_VALUE) min = -1;
         System.out.println(min);
         close();
     }
